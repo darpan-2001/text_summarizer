b772d3a81a45391baf6e0d2ca0661e0fcb37ad8a
Darpan Chanana <darpanchanana512@gmail.com>
2025-01-26 16:03:45 +0530

    DATA VALIDATION component added

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from src.text_summarizer.logging import logger
 from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from text_summarizer.pipeline.data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME="DATA INGESTION STAGE"
+# STAGE_NAME="DATA INGESTION STAGE"
 
+# try:
+#     logger.info(f"{STAGE_NAME} started............")
+#     data_ingestion = DataIngestionTrainingPipeline()
+#     data_ingestion.main()
+#     logger.info(f"{STAGE_NAME} completed.")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Data Validation stage"
 try:
-    logger.info(f"{STAGE_NAME} started............")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f"{STAGE_NAME} completed.")
+   logger.info(f"{STAGE_NAME} started............")
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f"{STAGE_NAME} completed.")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
 
 # logger.info("this is s custom log")
--- src/text_summarizer/config/configuration.py
+++ src/text_summarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from text_summarizer.constants import *
 from text_summarizer.utils.common import read_yaml, create_directories
 from text_summarizer.entity import DataIngestionConfig
+from text_summarizer.entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(self, config_file_path = CONFIG_FILE_PATH, params_file_path = PARAMS_FILE_PATH):
@@ -23,3 +24,16 @@ class ConfigurationManager:
             return data_ingestion_config
         except KeyError as e:
             raise ValueError(f"Missing key in data_ingestion configuration: {e}")
+        
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES,
+        )
+        return data_validation_config
