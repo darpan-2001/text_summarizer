36456c41886924169afa8a529c40c9153236d2b9
Darpan Chanana <darpanchanana512@gmail.com>
2025-01-26 19:24:45 +0530

    DATA VALIDATION component added

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from src.text_summarizer.logging import logger
 from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.pipeline.data_validation import DataValidationTrainingPipeline
 
-# STAGE_NAME="DATA INGESTION STAGE"
+STAGE_NAME="DATA INGESTION STAGE"
 
-# try:
-#     logger.info(f"{STAGE_NAME} started............")
-#     data_ingestion = DataIngestionTrainingPipeline()
-#     data_ingestion.main()
-#     logger.info(f"{STAGE_NAME} completed.")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+try:
+    logger.info(f"{STAGE_NAME} started............")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{STAGE_NAME} completed.")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Data Validation stage"
 try:
