40226b92bc995b31a1a59153a298faac40f36b25
Darpan Chanana <darpanchanana512@gmail.com>
2025-01-26 20:19:54 +0530

    DATA TRANSFORMATION component added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.text_summarizer.logging import logger
 from text_summarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from text_summarizer.pipeline.data_validation import DataValidationTrainingPipeline
+from text_summarizer.pipeline.data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME="DATA INGESTION STAGE"
 
@@ -18,6 +19,17 @@ try:
    logger.info(f"{STAGE_NAME} started............")
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f"{STAGE_NAME} completed.")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f"{STAGE_NAME} started............")
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f"{STAGE_NAME} completed.")
 except Exception as e:
         logger.exception(e)
